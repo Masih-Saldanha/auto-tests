ae7eb452fd4cbd97fc34e664e327eead33ac83b7
Masih-Saldanha <masih.saldanha@gmail.com>
2022-08-08 21:25:00 -0300

    feat: add auto deploy and Route GET'/1'

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("Olá, T5!");
 });
 
+app.get("/1", (req, res) => {
+  res.send("Teste 1");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
